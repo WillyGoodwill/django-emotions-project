fb987c85eb1a57c0eb806e3f6608391d072c3ca1
OLGA LAZUNINA <olga.lazunina@gmail.com>
2020-12-16 21:46:06 +0300

    code without stocks and fixed about me href

--- basic_app/urls.py
+++ basic_app/urls.py
@@ -9,7 +9,6 @@ app_name = 'basic_app'
 
 urlpatterns = [
     url(r'^$', views.index,name = 'index'),
-    # url(r'^emotions/$', views.emotions,name = 'emotions'),
     url(r'^emotions_form/$', views.emotions_form,name = 'emotions_form'),
 
     # url(r'^login/$', views.login,name = 'login'),
@@ -18,9 +17,6 @@ urlpatterns = [
     path('signup/', views.signupuser,name = 'signup'),
     url(r'^vis/$', views.vis,name = 'vis'),
     url(r'^emotions/$', views.emotions,name = 'emotions'),
-    url(r'^stocks/$', views.stocks,name = 'stocks'),
-    url(r'^add_stock/$', views.add_stock,name = 'add_stock'),
-    url(r'^delete/<stock_id>$', views.delete,name = 'delete'),
     url(r'^test_about_me/$', views.about_me,name = 'about_me'),
     path('delete_about_me/<int:text_id>', views.delete_about_me,name = 'delete_about_me'),
     path('delete_about_me_others/<int:text_id>', views.delete_about_me_others,name = 'delete_about_me_others'),
